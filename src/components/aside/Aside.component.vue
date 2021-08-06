1835ea38dae673280c7329669e7b5a6c4746c311
wjbaker101 <willbaker10198@gmail.com>
2021-08-06 22:13:25 +0100

    This has been moved to the manage feeds modal

--- src/components/aside/Aside.component.vue
+++ src/components/aside/Aside.component.vue
@@ -9,77 +9,36 @@
             </section>
             <section>
                 <FeedSourceComponent
-                    :key="source.reference"
-                    v-for="source in feeds"
-                    :source="source"
+                    :key="feed.reference"
+                    v-for="feed in feeds"
+                    :source="feed"
                 />
-            </section>
-            <section>
-                <label>
-                    <strong>New Feed</strong>
-                    <div class="flex gap">
-                        <input type="text" placeholder="https://example.com/rss" v-model="newFeed">
-                        <button class="flex-auto" @click="onAddFeed">Add</button>
-                    </div>
-                </label>
-                <UserMessageComponent :message="newFeedUserMessage" />
             </section>
         </div>
     </aside>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 import FeedSourceComponent from '@/components/aside/FeedSource.component.vue';
-import UserMessageComponent from '@/components/UserMessage.component.vue';
 
 import { UseRss } from '@/use/Rss.use';
-import { UseUserMessage } from '@/use/UserMessage.use';
-
-import { feedService } from '@/service/Feed.service';
 
 export default defineComponent({
     name: 'AsideComponent',
 
     components: {
         FeedSourceComponent,
-        UserMessageComponent,
     },
 
     setup() {
         const useRss = UseRss();
-        const useUserMessage = UseUserMessage();
 
         const feeds = useRss.feeds;
 
-        const newFeed = ref<string>('');
-        const newFeedUserMessage = ref<string>('');
-
         return {
             feeds,
-            newFeed,
-            newFeedUserMessage,
-
-            async onAddFeed() {
-                if (newFeed.value.length < 5) {
-                    useUserMessage.set(newFeedUserMessage, 'Please enter a valid feed URL and try again.');
-                    return;
-                }
-
-                const addFeedResponse = await feedService.addFeedSource({
-                    url: newFeed.value,
-                });
-
-                if (addFeedResponse instanceof Error) {
-                    useUserMessage.set(newFeedUserMessage, addFeedResponse.message);
-                    return;
-                }
-
-                useRss.articles.value = useRss.articles.value?.concat(addFeedResponse) ?? null;
-
-                newFeed.value = '';
-            },
         }
     },
 });
