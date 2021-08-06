c422564e0b55d000d7fa7a0dfed8142770fab3c7
wjbaker101 <willbaker10198@gmail.com>
2021-08-06 18:53:44 +0100

    Display feeds and their collection

--- src/components/modal/content/ManageFeeds.modal.component.vue
+++ src/components/modal/content/ManageFeeds.modal.component.vue
@@ -1,37 +1,107 @@
 <template>
     <SideModalContentComponent @close="onClose">
         <h2>Manage Feeds</h2>
+        <section>
+            <label>
+                <strong>New Feed</strong>
+                <div class="flex gap">
+                    <input type="text" placeholder="https://example.com/rss" v-model="newFeed">
+                    <button class="flex-auto" @click="onAddFeed">Add</button>
+                </div>
+            </label>
+            <UserMessageComponent :message="newFeedUserMessage" />
+        </section>
+        <section>
+            <div :key="`collection-${collection}`" v-for="(feeds, collection) in feedsByCollection">
+                <h3>{{ collection === 'mighty-rss-no-collection' ? 'Feeds with no Collection' : collection }}</h3>
+                <div :key="feed.reference" v-for="feed in feeds">
+                    {{ feed.title }}
+                </div>
+            </div>
+        </section>
     </SideModalContentComponent>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
 import SideModalContentComponent from '@/components/modal/SideModalContent.component.vue';
+import UserMessageComponent from '@/components/UserMessage.component.vue';
+
+import { feedService } from '@/service/Feed.service';
+import { UseRss } from '@/use/Rss.use';
+import { UseUserMessage } from '@/use/UserMessage.use';
+
+import { FeedSource } from '@/types/FeedSource.type';
 
 export default defineComponent({
     name: 'ManageFeedsModalComponent',
 
     components: {
         SideModalContentComponent,
+        UserMessageComponent,
     },
 
     setup(_, { emit }) {
+        const useRss = UseRss();
+        const useUserMessage = UseUserMessage();
+
+        const feeds = useRss.feeds;
+
+        const newFeed = ref<string>('');
+        const newFeedUserMessage = ref<string>('');
+
+        const feedsByCollection = computed<Record<string, Array<FeedSource>> | null>(() => {
+            if (feeds.value === null)
+                return null;
+
+            const value: Record<string, Array<FeedSource>> = {};
+
+            for (const feed of feeds.value) {
+                const collection = feed.collection ?? 'mighty-rss-no-collection';
+
+                if (collection in value) {
+                    value[collection].push(feed);
+                    continue;
+                }
+
+                value[collection] = [ feed ];
+            }
+
+            return value;
+        });
+
         const sharedAction = function () {
             emit('close');
         };
 
         return {
-            onYes() {
-                sharedAction();
-            },
-
-            onNo() {
-                sharedAction();
-            },
+            newFeed,
+            newFeedUserMessage,
+            feedsByCollection,
 
             onClose() {
                 sharedAction();
+            },
+
+            async onAddFeed() {
+                if (newFeed.value.length < 5) {
+                    useUserMessage.set(newFeedUserMessage, 'Please enter a valid feed URL and try again.');
+                    return;
+                }
+
+                const addFeedResponse = await feedService.addFeedSource({
+                    url: newFeed.value,
+                });
+
+                if (addFeedResponse instanceof Error) {
+                    useUserMessage.set(newFeedUserMessage, addFeedResponse.message);
+                    return;
+                }
+
+                useRss.articles.value = useRss.articles.value?.concat(addFeedResponse) ?? null;
+
+                newFeed.value = '';
             },
         }
     },
