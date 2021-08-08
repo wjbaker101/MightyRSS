c5842f54b38da5a9765ec2fea2c4caa422c2d1e5
wjbaker101 <willbaker10198@gmail.com>
2021-08-08 14:07:34 +0100

    Add the rest of the repositories

--- Data/UoW/MightyUnitOfWork.cs
+++ Data/UoW/MightyUnitOfWork.cs
@@ -6,15 +6,27 @@ namespace MightyRSS.Data.UoW
     public interface IMightyUnitOfWork : IUnitOfWork
     {
         public IFeedSourceRepositoryV2 FeedSources { get; }
+
+        public IUserFeedSourceRepositoryV2 UserFeedSources { get; }
+
+        public IUserRepositoryV2 Users { get; }
     }
 
     public sealed class MightyUnitOfWork : UnitOfWork, IMightyUnitOfWork
     {
         public IFeedSourceRepositoryV2 FeedSources { get; }
 
+        public IUserFeedSourceRepositoryV2 UserFeedSources { get; }
+
+        public IUserRepositoryV2 Users { get; }
+
         public MightyUnitOfWork(IApiDatabase database) : base(database)
         {
             FeedSources = new FeedSourceRepositoryV2(Session);
+
+            UserFeedSources = new UserFeedSourceRepositoryV2(Session);
+
+            Users = new UserRepositoryV2(Session);
         }
     }
 }
