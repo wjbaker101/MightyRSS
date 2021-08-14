b1c16488b5ff95538151d3b9459206dc2fe1b163
wjbaker101 <willbaker10198@gmail.com>
2021-08-14 20:36:23 +0100

    Fix side modal being translated incorrectly on first showing

--- src/components/modal/SideModalContent.component.vue
+++ src/components/modal/SideModalContent.component.vue
@@ -93,11 +93,11 @@ export default defineComponent({
     @keyframes open {
         0% {
             opacity: 0;
-            transform: translate(100%, -50%);
+            transform: translate(100%, 0);
         }
         100% {
             opacity: 1;
-            transform: translate(0, -50%);
+            transform: translate(0, 0);
         }
     }
 }
