64f74a75374b562f5756992e0f147ee44520ab51
wjbaker101 <willbaker10198@gmail.com>
2021-08-21 12:48:44 +0100

    Add logic for navigating between login inputs and add enter action

--- frontend/src/components/login/Login.component.vue
+++ frontend/src/components/login/Login.component.vue
@@ -4,11 +4,21 @@
         <section>
             <label>
                 <strong>Username</strong>
-                <input type="text" placeholder="" v-model="username">
+                <input
+                    ref="usernameInput"
+                    type="text"
+                    v-model="username"
+                    @keyup.enter="onUsernameEnter"
+                >
             </label>
             <label>
                 <strong>Password</strong>
-                <input type="password" placeholder="" v-model="password">
+                <input
+                    ref="passwordInput"
+                    type="password"
+                    v-model="password"
+                    @keyup.enter="onPasswordEnter"
+                >
             </label>
         </section>
         <section>
@@ -45,30 +55,66 @@ export default defineComponent({
 
         const loginToken = useLoginToken.loginToken;
 
+        const usernameInput = ref<HTMLInputElement | null>(null);
+        const passwordInput = ref<HTMLInputElement | null>(null);
+
         const username = ref<string>('');
         const password = ref<string>('');
         const userMessage = ref<string>('');
 
+        const logIn = async function () {
+            if (username.value.length < 3) {
+                useUserMessage.set(userMessage, 'Username is not valid, please try again.');
+                return;
+            }
+            if (password.value.length < 3) {
+                useUserMessage.set(userMessage, 'Password is not valid, please try again.');
+                return;
+            }
+
+            const logInResponse = await authService.logIn({
+                username: username.value,
+                password: password.value,
+            });
+            if (logInResponse instanceof Error) {
+                useUserMessage.set(userMessage, logInResponse.message);
+                return;
+            }
+
+            loginToken.value = logInResponse;
+
+            await cacheService.set(CacheKey.LOGIN_TOKEN, loginToken.value);
+
+            emit('login');
+        };
+
         return {
+            usernameInput,
+            passwordInput,
             username,
             password,
             userMessage,
 
             async onLogIn() {
-                const logInResponse = await authService.logIn({
-                    username: username.value,
-                    password: password.value,
-                });
-                if (logInResponse instanceof Error) {
-                    useUserMessage.set(userMessage, logInResponse.message);
+                await logIn();
+            },
+
+            async onUsernameEnter() {
+                if (password.value.length === 0) {
+                    passwordInput.value?.focus();
                     return;
                 }
 
-                loginToken.value = logInResponse;
+                await logIn();
+            },
 
-                await cacheService.set(CacheKey.LOGIN_TOKEN, loginToken.value);
+            async onPasswordEnter() {
+                if (username.value.length === 0) {
+                    usernameInput.value?.focus();
+                    return;
+                }
 
-                emit('login');
+                await logIn();
             },
         }
     },
