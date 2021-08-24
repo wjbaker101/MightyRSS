45f30f2ba157e582d53e19ab8447bd2ddd784e08
wjbaker101 <willbaker10198@gmail.com>
2021-08-24 13:07:50 +0100

    Invalidate the login token when the api responds with a unauthorised

--- frontend/src/api/ResponseHelper.ts
+++ frontend/src/api/ResponseHelper.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
+import { UseLoginToken } from '@/use/LoginToken.use';
+
+const useLoginToken = UseLoginToken();
+
+const UNAUTHORISED = 401;
+
 class ResponseHelper {
 
     handleError(error: any): Error {
-        if (axios.isAxiosError(error))
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === UNAUTHORISED)
+                useLoginToken.clear();
+
             return new Error(error.message);
+        }
 
         return new Error('Something went wrong.');
     }
