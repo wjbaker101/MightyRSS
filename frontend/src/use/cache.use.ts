a7dbed612943487015f632751fea6e14231721b9
wjbaker101 <willbaker10198@gmail.com>
2022-11-12 14:07:31 +0000

    New cache using, so immortal db is not required and it is not async

---
+++ frontend/src/use/cache.use.ts
@@ -0,0 +1,35 @@
+export const Keys: Record<string, string> = {
+
+};
+
+interface ICacheItem<T> {
+    data: T;
+    expiresAt: number;
+}
+
+export const useCache = function () {
+    return {
+
+        get<T>(key: string): T | null {
+            const storage = localStorage.getItem(key);
+            if (storage === null)
+                return null;
+
+            const parsed = JSON.parse(storage) as ICacheItem<T>;
+
+            if (parsed.expiresAt !== -1 && Date.now() > parsed.expiresAt)
+                return null;
+
+            return parsed.data;
+        },
+
+        set<T>(key: string, data: T, expiresAt: number = -1): void {
+            const cacheItem: ICacheItem<T> = {
+                data,
+                expiresAt,
+            };
+            localStorage.setItem(key, JSON.stringify(cacheItem));
+        },
+
+    };
+};
