f71932031fee34abc0a5bfbc3b6792186c74c68c
wjbaker101 <willbaker10198@gmail.com>
2023-01-26 17:17:42 +0000

    Basic configuration view content

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
+import ConfigurationView from '@/view/configuration/Configuration.view.vue';
 import FeedView from '@/view/feed/Feed.view.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         component: FeedView,
+    },
+    {
+        path: '/configuration',
+        component: ConfigurationView,
     },
 ];
 
---
+++ frontend/src/view/configuration/Configuration.view.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="configuration-view">
+        <HeaderComponent />
+        <div class="content-width">
+            <div class="user-details content-container">
+                <h2>Currently logged in as:</h2>
+                <p>TestUsername</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import HeaderComponent from '@/components/Header.component.vue';
+</script>
+
+<style lang="scss">
+.configuration-view {
+
+    .content-container {
+        padding: 1rem;
+        border-radius: 0.5rem;
+        box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2), 2px 3px 30px rgba(0, 0, 0, 0.2), inset 0 0 3px rgba(0, 0, 0, 0.2);
+    }
+}
+</style>
