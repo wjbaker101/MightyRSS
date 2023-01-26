52094d7d0452897e0a0c35acccaa48b02b96961c
wjbaker101 <willbaker10198@gmail.com>
2023-01-26 17:04:09 +0000

    Add vue-router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,9 @@ import '@/setup/ServiceWorker';
 import { components } from '@/setup/components';
 
 import App from '@/App.vue';
+import { router } from '@/router/router';
 
 const app = createApp(App);
+app.use(router);
 app.use(components);
 app.mount('#app');
