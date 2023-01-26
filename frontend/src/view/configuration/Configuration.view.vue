9a97426a918052b1e6e21a2ae0cc4863b0c659d9
wjbaker101 <willbaker10198@gmail.com>
2023-01-26 19:36:09 +0000

    Configuration user content

--- frontend/src/view/configuration/Configuration.view.vue
+++ frontend/src/view/configuration/Configuration.view.vue
@@ -2,9 +2,14 @@
     <div class="configuration-view">
         <HeaderComponent />
         <div class="content-width">
-            <div class="user-details content-container">
-                <h2>Currently logged in as:</h2>
-                <p>TestUsername</p>
+            <div class="user-details content-container flex align-items-center">
+                <div class="flex-2">
+                    <h2>Currently logged in as:</h2>
+                    <p>TestUsername</p>
+                </div>
+                <div class="text-centered">
+                    <strong class="feed-count">5</strong> feeds
+                </div>
             </div>
         </div>
     </div>
@@ -18,16 +23,14 @@ import HeaderComponent from '@/components/Header.component.vue';
 .configuration-view {
 
     .content-container {
-        padding: 2rem 1rem;
+        padding: 1rem;
         border-radius: 0.5rem;
         box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.2), 2px 3px 30px rgba(0, 0, 0, 0.2), inset 0 0 3px rgba(0, 0, 0, 0.2);
+    }
 
-        & > :first-child {
-            margin-top: 0;
-        }
-
-        & > :last-child {
-            margin-bottom: 0;
+    .user-details {
+        .feed-count {
+            font-size: 2rem;
         }
     }
 }
