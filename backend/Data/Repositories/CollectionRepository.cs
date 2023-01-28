40f944d2a497e6e0b6a90ec9feaa94d4eb5d130a
wjbaker101 <willbaker10198@gmail.com>
2023-01-28 16:51:00 +0000

    Add method for getting a collection by reference

--- backend/Data/Repositories/CollectionRepository.cs
+++ backend/Data/Repositories/CollectionRepository.cs
@@ -1,16 +1,30 @@
 ﻿using Data.Records;
 using Data.UoW;
+using NetApiLibs.Type;
 using NHibernate;
 
 namespace Data.Repositories;
 
 public interface ICollectionRepository : IRepository<FeedSourceRecord>
 {
+    Result<CollectionRecord> GetByReference(Guid collectionReference);
 }
 
 public sealed class CollectionRepository : Repository<FeedSourceRecord>, ICollectionRepository
 {
     public CollectionRepository(ISession session) : base(session)
     {
+    }
+
+    public Result<CollectionRecord> GetByReference(Guid collectionReference)
+    {
+        var collection = Session
+            .Query<CollectionRecord>()
+            .SingleOrDefault(x => x.Reference == collectionReference);
+
+        if (collection == null)
+            return Result<CollectionRecord>.Failure($"Unable to find collection with reference: {collectionReference}.");
+
+        return collection;
     }
 }
