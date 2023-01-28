0647b89e5ab242481d52b91c9174b9684b1c482c
wjbaker101 <willbaker10198@gmail.com>
2023-01-28 16:39:06 +0000

    Create collection Record/Model

---
+++ backend/Core/Models/CollectionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models;
+
+public sealed class CollectionModel
+{
+    public required Guid Reference { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required string Name { get; init; }
+}
---
+++ backend/Data.Records/CollectionRecord.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Data.Records;
+
+public class CollectionRecord
+{
+    public long Id { get; init; }
+    public required Guid Reference { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required UserRecord User { get; init; }
+    public required string Name { get; init; }
+}
+
+public sealed class CollectionRecordMap : ClassMap<CollectionRecord>
+{
+    public CollectionRecordMap()
+    {
+        Schema("mighty_rss");
+        Table("collection");
+        Id(x => x.Id, "id").GeneratedBy.SequenceIdentity("collection_id_seq");
+        Map(x => x.Reference, "reference");
+        Map(x => x.CreatedAt, "created_at");
+        References(x => x.User, "user_id");
+        Map(x => x.Name, "name");
+    }
+}
