c95f37a6d8919713d4e2b38c60c9ea894060e779
wjbaker101 <willbaker10198@gmail.com>
2023-01-28 16:45:40 +0000

    Create `CollectionRepository`

---
+++ backend/Data/Repositories/CollectionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Records;
+using Data.UoW;
+using NHibernate;
+
+namespace Data.Repositories;
+
+public interface ICollectionRepository : IRepository<FeedSourceRecord>
+{
+}
+
+public sealed class CollectionRepository : Repository<FeedSourceRecord>, ICollectionRepository
+{
+    public CollectionRepository(ISession session) : base(session)
+    {
+    }
+}
--- backend/Data/UoW/MightyUnitOfWork.cs
+++ backend/Data/UoW/MightyUnitOfWork.cs
@@ -5,26 +5,23 @@ namespace Data.UoW;
 public interface IMightyUnitOfWork : IUnitOfWork
 {
     public IFeedSourceRepository FeedSources { get; }
-
     public IUserFeedSourceRepository UserFeedSources { get; }
-
     public IUserRepository Users { get; }
+    public ICollectionRepository Collections { get; }
 }
 
 public sealed class MightyUnitOfWork : UnitOfWork, IMightyUnitOfWork
 {
     public IFeedSourceRepository FeedSources { get; }
-
     public IUserFeedSourceRepository UserFeedSources { get; }
-
     public IUserRepository Users { get; }
+    public ICollectionRepository Collections { get; }
 
     public MightyUnitOfWork(IApiDatabase database) : base(database)
     {
         FeedSources = new FeedSourceRepository(Session);
-
         UserFeedSources = new UserFeedSourceRepository(Session);
-
         Users = new UserRepository(Session);
+        Collections = new CollectionRepository(Session);
     }
 }
