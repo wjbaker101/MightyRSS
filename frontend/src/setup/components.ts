450dfbfbcbd7566ee7c0768091c496dd1209dc62
wjbaker101 <willbaker10198@gmail.com>
2023-01-30 23:57:39 +0000

    Add more @wjb/vue components

--- frontend/src/setup/components.ts
+++ frontend/src/setup/components.ts
@@ -3,6 +3,9 @@ import { Plugin } from 'vue';
 import ButtonComponent from '@wjb/vue/component/ButtonComponent.vue';
 import IconComponent from '@wjb/vue/component/IconComponent.vue';
 import ModalComponent from '@wjb/vue/component/ModalComponent.vue';
+import FormComponent from '@wjb/vue/component/form/FormComponent.vue';
+import FormSectionComponent from '@wjb/vue/component/form/FormSectionComponent.vue';
+import FormInputComponent from '@wjb/vue/component/form/FormInputComponent.vue';
 
 export const components: Plugin = {
 
@@ -10,6 +13,9 @@ export const components: Plugin = {
         app.component('ButtonComponent', ButtonComponent);
         app.component('IconComponent', IconComponent);
         app.component('ModalComponent', ModalComponent);
+        app.component('FormComponent', FormComponent);
+        app.component('FormSectionComponent', FormSectionComponent);
+        app.component('FormInputComponent', FormInputComponent);
     },
 
 };
