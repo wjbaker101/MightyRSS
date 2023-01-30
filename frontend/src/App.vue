2f0d49648d61474dd6cb60a4d35a00d81cded9b4
wjbaker101 <willbaker10198@gmail.com>
2023-01-30 23:49:22 +0000

    Add ModalComponent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <RouterView></RouterView>
+    <ModalComponent />
 </template>
 
 <script setup lang="ts">
--- frontend/src/setup/components.ts
+++ frontend/src/setup/components.ts
@@ -1,11 +1,15 @@
 import { Plugin } from 'vue';
 
+import ButtonComponent from '@wjb/vue/component/ButtonComponent.vue';
 import IconComponent from '@wjb/vue/component/IconComponent.vue';
+import ModalComponent from '@wjb/vue/component/ModalComponent.vue';
 
 export const components: Plugin = {
 
     install(app) {
+        app.component('ButtonComponent', ButtonComponent);
         app.component('IconComponent', IconComponent);
+        app.component('ModalComponent', ModalComponent);
     },
 
 };
