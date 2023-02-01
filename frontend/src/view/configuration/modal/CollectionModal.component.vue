a01f0c5ba1e52759af69a04b9e1334ece2fb74cc
wjbaker101 <willbaker10198@gmail.com>
2023-02-01 17:48:27 +0000

    Add possibly passed in collection

--- frontend/src/view/configuration/modal/CollectionModal.component.vue
+++ frontend/src/view/configuration/modal/CollectionModal.component.vue
@@ -1,20 +1,36 @@
 <template>
     <div class="collection-modal-component">
-        <h2>New Collection</h2>
+        <h2>{{ form.reference ? 'Update' : 'New' }} Collection</h2>
         <FormComponent>
             <FormSectionComponent>
                 <FormInputComponent label="Name">
-                    <input type="text" placeholder="My Collection">
+                    <input type="text" placeholder="My Collection" v-model="form.name">
                 </FormInputComponent>
             </FormSectionComponent>
             <FormSectionComponent>
-                <button>asd</button>
+                <button>{{ form.reference ? 'Update' : 'Create' }}</button>
             </FormSectionComponent>
         </FormComponent>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ICollection } from '@/model/Collection.model';
+import { ref } from 'vue';
+
+const props = defineProps<{
+    collection?: ICollection;
+}>();
+
+interface IForm {
+    reference?: string;
+    name: string;
+}
+
+const form = ref<IForm>({
+    reference: props.collection?.reference,
+    name: props.collection?.name ?? '',
+});
 </script>
 
 <style lang="scss">
