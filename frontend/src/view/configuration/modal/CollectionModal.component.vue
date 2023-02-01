cff864fa43746961705c4a53a8750270b6c52dab
wjbaker101 <willbaker10198@gmail.com>
2023-02-01 17:34:07 +0000

    Create modal for createing/updating collections

---
+++ frontend/src/view/configuration/modal/CollectionModal.component.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="collection-modal-component">
+        <h2>New Collection</h2>
+        <FormComponent>
+            <FormSectionComponent>
+                <FormInputComponent label="Name">
+                    <input type="text" placeholder="My Collection">
+                </FormInputComponent>
+            </FormSectionComponent>
+            <FormSectionComponent>
+                <button>asd</button>
+            </FormSectionComponent>
+        </FormComponent>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.collection-modal-component {
+    width: 540px;
+}
+</style>
