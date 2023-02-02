a1fbb9b2ecac47b26684ee3af22e74f99a906706
wjbaker101 <willbaker10198@gmail.com>
2023-02-02 22:54:30 +0000

    Create feed moda

---
+++ frontend/src/view/configuration/modal/FeedModal.component.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="feed-modal-component">
+        <h2>{{ form.reference ? 'Update' : 'New' }} Feed</h2>
+        <FormComponent>
+            <FormSectionComponent>
+                <FormInputComponent label="Url">
+                    <input type="text" placeholder="https://example.com/rss" v-model="form.websiteUrl">
+                </FormInputComponent>
+            </FormSectionComponent>
+            <FormSectionComponent>
+                <button @click="onSubmit">{{ form.reference ? 'Update' : 'Create' }}</button>
+            </FormSectionComponent>
+        </FormComponent>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IFeedSource } from '@/model/FeedSource.model';
+import { ref } from 'vue';
+
+const props = defineProps<{
+    feedSource?: IFeedSource;
+}>();
+
+interface IForm {
+    reference?: string;
+    title: string;
+    websiteUrl: string;
+}
+
+const form = ref<IForm>({
+    reference: props.feedSource?.reference,
+    title: props.feedSource?.title ?? '',
+    websiteUrl: props.feedSource?.websiteUrl ?? '',
+});
+
+const onSubmit = function (): void {};
+</script>
+
+<style lang="scss">
+.feed-modal-component {
+    width: 540px;
+}
+</style>
