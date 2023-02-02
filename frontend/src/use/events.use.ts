05f4a6a79a84ae88335f4d5880eccf05a5639dcc
wjbaker101 <willbaker10198@gmail.com>
2023-02-02 19:01:16 +0000

    useEvents changes

--- frontend/src/use/events.use.ts
+++ frontend/src/use/events.use.ts
@@ -1,20 +1,21 @@
 export type EventKey =
     'ON_LOG_IN';
 
-type EventFunction = (params: object) => void;
+type EventFunction<T> = (params: T) => void;
 
-const events = new Map<EventKey, Array<EventFunction>>();
-events.set('ON_LOG_IN', []);
+const events: Record<EventKey, Array<EventFunction<any>>> = {
+    ON_LOG_IN: [],
+};
 
 export const useEvents = function () {
     return {
 
-        publish(key: EventKey, params: object): void {
-            events.get(key)?.forEach(x => x(params))
+        publish<T>(key: EventKey, params: T): void {
+            events[key].forEach(x => x(params))
         },
 
-        subscribe(key: EventKey, func: EventFunction): void {
-            events.get(key)?.push(func);
+        subscribe<T>(key: EventKey, func: EventFunction<T>): void {
+            events[key].push(func);
         },
 
     };
