c8d5890aac0cc349cd72f360d532922e51d26095
wjbaker101 <willbaker10198@gmail.com>
2023-02-05 11:31:33 +0000

    Redirect to the login page if no loginToken exists

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,13 +1,25 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, NavigationGuardWithThis, RouteRecordRaw } from 'vue-router';
 
 import ConfigurationView from '@/views/configuration/Configuration.view.vue';
 import LoginView from '@/views/login/Login.view.vue';
 import FeedView from '@/views/feed/Feed.view.vue';
 
+import { useAppData } from '@/use/app-data.use';
+
+const appData = useAppData();
+
+const requireAuth: NavigationGuardWithThis<void> = function (to, from, next): void {
+    if (appData.auth.loginToken.value === null)
+        next('/login');
+    else
+        next();
+};
+
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         component: FeedView,
+        beforeEnter: [requireAuth],
     },
     {
         path: '/login',
@@ -16,6 +28,7 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/configuration',
         component: ConfigurationView,
+        beforeEnter: [requireAuth],
     },
 ];
 
