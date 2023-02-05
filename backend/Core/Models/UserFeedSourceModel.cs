8d10fe01ebacef60868347f528c37eadfeffc884
wjbaker101 <willbaker10198@gmail.com>
2023-02-05 17:41:41 +0000

    Delete unused model

--- backend/Core/Models/UserFeedSourceModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Core.Models;
-
-public sealed class UserFeedSourceModel
-{
-    public required string? Collection { get; init; }
-    public required string? TitleAlias { get; init; }
-}
