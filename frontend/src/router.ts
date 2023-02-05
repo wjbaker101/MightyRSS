8a18d023eb5bae3e3c46f9fc50e37d291eab2d75
wjbaker101 <willbaker10198@gmail.com>
2023-02-05 10:17:57 +0000

    Create login view

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 import ConfigurationView from '@/views/configuration/Configuration.view.vue';
+import LoginView from '@/views/login/Login.view.vue';
 import FeedView from '@/views/feed/Feed.view.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         component: FeedView,
+    },
+    {
+        path: '/login',
+        component: LoginView,
     },
     {
         path: '/configuration',
